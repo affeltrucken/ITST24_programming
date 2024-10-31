6774535f973e26f6fb0d095548c50b9b04ed40c3
affeltrucken <smajlovicaldin52@gmail.com>
2024-10-31 02:07:53 +0100

    fixed bugs

---
+++ python/main_project/cryptonite/general_tools.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from rich.console import Console
+from rich.prompt import Prompt
+
+console = Console()
+
+def read_file(filename: str, read_mode: str = "r") -> bytes:
+    with open(filename, read_mode) as file:
+        return file.read()
+    
+def yes_no(prompt: str) -> bool:
+    while True:
+        answer = Prompt.ask(f"{prompt} (y/n)").strip().lower()
+        if answer in ['y', 'n']:
+            return answer == 'y'
+        console.print("Please enter 'y' or 'n'.")
+        
+def write_to_file(filename: str, data: str, mode: str = "a") -> None:
+    with open(filename, mode, encoding="utf-8") as file:
+        file.write(data)
+
+def ask_overwrite(filename: str) -> bool:
+    return not Path(filename).exists() or yes_no(f"{filename} exists. Overwrite?")
+
+from pathlib import Path
+
+def input_file(prompt_text: str = "", ask_overwrite: bool = True, file_exists_required: bool = True) -> str:
+    while True:
+        filename = Prompt.ask(f"Filename{f' ({prompt_text})' if prompt_text else ''}")
+        file_path = Path(filename)
+        
+        # File does not exist
+        if not file_path.exists():
+            if file_exists_required:
+                console.print("File does not exist.")
+                continue
+            return filename
+
+        # File exists
+        if not ask_overwrite:  # Overwrite is not permitted
+            return filename  # Just return the filename since we know it exists
+        elif yes_no(f"{filename} already exists. Overwrite?"):
+            return filename
+
+
+
+def main():
+    print("This is not meant to run directly. It is intended to be used as a library.")
+
+if __name__ == "__main__":
+    main()
